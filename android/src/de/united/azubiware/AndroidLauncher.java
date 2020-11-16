0444aff4075cf25111a212ea3847e3a9b1d34e0a
NouCake <KevinFischer2010@gmail.com>
2020-11-16 11:27:07 +0100

    init libgdx project

---
+++ android/src/de/united/azubiware/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.united.azubiware;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.united.azubiware.AzubiWareMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AzubiWareMain(), config);
+	}
+}
