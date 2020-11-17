61575be419d6b40a356f5ebeaf44aef69cbc94ac
NouCake <KevinFischer2010@gmail.com>
2020-11-17 16:03:12 +0100

    build.gradle refactored modules + dependencies

--- core/src/de/united/azubiware/AzubiWareMain.java
+++ core/src/de/united/azubiware/AzubiWareMain.java
@@ -4,15 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.ui.Container;
-import org.java_websocket.client.WebSocketClient;
-import org.java_websocket.handshake.ServerHandshake;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class AzubiWareMain extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -22,39 +14,6 @@ public class AzubiWareMain extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		try {
-
-			WebSocketClient socket = new WebSocketClient(new URI("ws://localhost:13030")) {
-				@Override
-				public void onOpen(ServerHandshake handshakedata) {
-					System.out.println("Connected");
-				}
-
-				@Override
-				public void onMessage(String message) {
-					System.out.println(message);
-					try{
-						int newX = Integer.parseInt(message);
-						x = newX;
-					} catch (NumberFormatException e){
-						e.printStackTrace();
-					}
-				}
-
-				@Override
-				public void onClose(int code, String reason, boolean remote) {
-					System.out.println("Closed");
-				}
-
-				@Override
-				public void onError(Exception ex) {
-					System.out.println("Error");
-				};
-			};
-			socket.connect();
-		} catch (URISyntaxException e) {
-			e.printStackTrace();
-		}
 
 	}
 
