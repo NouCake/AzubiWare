4cceeee08ee08e2f766f5428acaaa00bd1a4d52b
NouCake <KevinFischer2010@gmail.com>
2020-11-25 10:06:57 +0100

    .

--- core/src/de/united/azubiware/screens/minigames/pong/PongScreen.java
+++ core/src/de/united/azubiware/screens/minigames/pong/PongScreen.java
@@ -1,7 +1,14 @@
 package de.united.azubiware.screens.minigames.pong;
 
 import com.badlogic.gdx.ScreenAdapter;
+import de.united.azubiware.AzubiWareGame;
+import de.united.azubiware.User.IUser;
+import de.united.azubiware.screens.minigames.MinigameBaseScreen;
 
-public class PongScreen extends ScreenAdapter {
+public class PongScreen extends MinigameBaseScreen {
+
+    public PongScreen(AzubiWareGame game, IUser... oponents) {
+        super(game, oponents);
+    }
 
 }
