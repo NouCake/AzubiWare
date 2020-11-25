d3107c1fc097c71ff62484a3a947e08d5a98695b
NouCake <KevinFischer2010@gmail.com>
2020-11-25 13:18:45 +0100

    Implemented Pong in Client

---
+++ core/src/de/united/azubiware/minigames/Pong.java
@@ -0,0 +1,34 @@
+package de.united.azubiware.minigames;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import de.united.azubiware.AzubiWareGame;
+import de.united.azubiware.Games.Pong.PongMatch;
+import de.united.azubiware.User.IUser;
+import de.united.azubiware.connection.match.IMatchListener;
+import de.united.azubiware.minigames.interfaces.IGame;
+import de.united.azubiware.screens.minigames.WaitingScreen;
+import de.united.azubiware.screens.minigames.pong.PongListener;
+import de.united.azubiware.screens.minigames.pong.PongScreen;
+
+public class Pong implements IGame {
+    @Override
+    public int getMatchType() {
+        return PongMatch.MATCH_TYPE;
+    }
+
+    @Override
+    public Texture getWaitingScreenBackground() {
+        return new Texture(Gdx.files.internal("backgrounds/backgroundForest.png"));
+    }
+
+    @Override
+    public Texture getSplash() {
+        return new Texture(Gdx.files.internal("games/pong/splash.png"));
+    }
+
+    @Override
+    public IMatchListener createMatchListener(WaitingScreen waitingScreen, AzubiWareGame game, IUser[] opponents) {
+        return new PongListener(new PongScreen(game, opponents[0]), waitingScreen);
+    }
+}
