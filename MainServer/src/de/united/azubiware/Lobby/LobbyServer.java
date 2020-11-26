ccd092bbba0e66c1a29d5846e84236ec5be9905c
dseydel <dseydel@united-internet.de>
2020-11-26 11:41:12 +0100

    Added SSPLobbyGame

--- MainServer/src/de/united/azubiware/Lobby/LobbyServer.java
+++ MainServer/src/de/united/azubiware/Lobby/LobbyServer.java
@@ -5,6 +5,7 @@ import de.united.azubiware.Connection.UserConnectionManager;
 import de.united.azubiware.Connection.WebSocket.IUserListener;
 import de.united.azubiware.Connection.WebSocket.WebSocketConnectionManager;
 import de.united.azubiware.Games.Pong.PongLobbyGame;
+import de.united.azubiware.Games.SSP.SSPLobbyGame;
 import de.united.azubiware.Games.TTT.TTTLobbyGame;
 import de.united.azubiware.Games.VG.VGLobbyGame;
 import de.united.azubiware.ILobbyGame;
@@ -36,6 +37,7 @@ public class LobbyServer implements ILobby, IUserListener {
         games.add(new TTTLobbyGame());
         games.add(new VGLobbyGame());
         games.add(new PongLobbyGame());
+        games.add(new SSPLobbyGame());
 
         userDB = new SimpleUserDatabase();
         UserConnectionManager connectionListener = new UserConnectionManager(this, userDB);
