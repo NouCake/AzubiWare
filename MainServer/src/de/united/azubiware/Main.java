fa382f54fcfd000818695ecccccc0342bc702f49
Ancocodet <ancoplays@gmail.com>
2022-02-11 09:56:49 +0100

    Change Server IP to Cloud one

--- MainServer/src/de/united/azubiware/Main.java
+++ MainServer/src/de/united/azubiware/Main.java
@@ -4,7 +4,7 @@ import de.united.azubiware.Lobby.LobbyServer;
 
 public class Main{
 
-    public static final String SERVER_DOMAIN = "reaved.net";
+    public static final String SERVER_DOMAIN = "138.68.124.234";
 
     public static void main(String[] args) {
         new LobbyServer();
